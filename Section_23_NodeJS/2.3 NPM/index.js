da27bd95052235e8aa7edff5f409f24dc91d1ec1
Amirreza Shams <amirrshams@gmail.com>
2024-05-14 11:48:54 -0400

    intro to npm

---
+++ Section_23_NodeJS/2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from "sillyName";
+import {randomSuperhero} from 'superheroes';
+
+var sillyName = generateName();
+const superHeroName = randomSuperhero()
+
+
+console.log(`my name is ${sillyName}.`);
+console.log(`I am ${superHeroName}!`);
